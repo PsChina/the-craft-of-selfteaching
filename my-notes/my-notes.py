f1a502d7cef20075f98bb9bbc492a32449a6f829
PsChina <panshanzxcv.love@163.com>
2019-04-11 00:37:29 +0800

    day2 了解 python 变量类型

--- my-notes/my-notes.py
+++ my-notes/my-notes.py
@@ -1,4 +1,5 @@
 # coding=UTF-8
+# day1
 # import random
 # r = random.randrange(1, 1000)
 # # 布尔运算
@@ -31,6 +32,7 @@
 #             break
 #     else:
 #         print(n)
+
 # # 所谓函数
 # a = abs(-3.1415926)
 # print(a)
@@ -45,4 +47,36 @@
 #     if is_prime(i):
 #         print(i)
 # # 细节补充
+
+# # day2
+
+# # 值及其相应的运算
+
+# # 值
+
+# #在 Python 中每个函数都有返回值，即便你在定义一个函数的时候没有设定返回值，它也会加上默认的返回值 None……（请注意 None 的大小写！）
+
+# #在不得不对不同类型的值进行运算之前，总是要事先做 Type Casting（类型转换）
+
+# #type()，可以用来查看某个值属于什么类型：
+
+# print( 
+#         type(1), 
+#         type('1'), 
+#         type(1.0), 
+#         type([1,2,3]), 
+#         type((1,2,3)), 
+#         type({1,2,3}),
+#         type({'a':'a','b':1,'c':True}),
+#         type(False),
+#         type(range(10)) # list ?
+#     )
+
+# # (<type 'int'>, <type 'str'>, <type 'float'>, <type 'list'>, <type 'tuple'>, <type 'set'>, <type 'dict'>, <type 'bool'>, <type 'list'>)
+
+# # 操作符 end 2019 4 11 12:36
+
+
+
+
 
